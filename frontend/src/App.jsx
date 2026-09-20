423bc07d8ef04e377a863e7d204f19147de70aa3
vladvin47 <cherry194415@gmail.com>
2026-09-20 18:33:20 +0300

    Add footer and footer import and add classNames for styles in App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,28 +6,38 @@ import FavoritesPage from './pages/FavoritesPage'
 import LoginPage from './pages/LoginPage'
 import RegisterPage from './pages/RegisterPage'
 import Header from './components/Header'
-
+import Footer from './components/Footer'
 
 function App() {
   return (
-    <>
+    <div className="app">
       <Header />
-      <Routes>
-        <Route path="/" element={<CatalogPage />} />
-        <Route path="/organizations/:id" element={<OrganizationDetailPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/register" element={<RegisterPage />} />
-        <Route
-          path="/favorites"
-          element={
-            <PrivateRoute>
-              <FavoritesPage />
-            </PrivateRoute>
-          }
-        />
-        <Route path="*" element={<p>Страница не найдена. <Link to="/">В каталог</Link></p>} />
-      </Routes>
-    </>
+      <main className="container">
+        <Routes>
+          <Route path="/" element={<CatalogPage />} />
+          <Route path="/organizations/:id" element={<OrganizationDetailPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route
+            path="/favorites"
+            element={
+              <PrivateRoute>
+                <FavoritesPage />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="*"
+            element={
+              <p className="state-message">
+                Страница не найдена. <Link to="/">В каталог</Link>
+              </p>
+            }
+          />
+        </Routes>
+      </main>
+      <Footer />
+    </div>
   )
 }
 
