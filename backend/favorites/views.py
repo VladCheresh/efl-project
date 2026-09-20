cd34b5105cd270e45deefdee97665c324ae7992c
vladvin47 <cherry194415@gmail.com>
2026-09-20 15:49:02 +0300

    Limit favorites API to create/list/delete and fix N+1 queries

--- backend/favorites/views.py
+++ backend/favorites/views.py
@@ -1,14 +1,19 @@
-from rest_framework import viewsets, permissions
+from rest_framework import viewsets, permissions, mixins
 from .models import Favorite
 from .serializers import FavoriteSerializer
 
 
-class FavoriteViewSet(viewsets.ModelViewSet):
+class FavoriteViewSet(mixins.CreateModelMixin,
+                      mixins.ListModelMixin,
+                      mixins.DestroyModelMixin,
+                      viewsets.GenericViewSet):
     serializer_class = FavoriteSerializer
     permission_classes = [permissions.IsAuthenticated]
 
     def get_queryset(self):
-        return Favorite.objects.filter(user=self.request.user)
+        return (Favorite.objects
+                .filter(user=self.request.user)
+                .select_related('organization__category'))
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
