b67ee62ceab93e7096c7f8e803e7bd0800eca133
vladvin47 <cherry194415@gmail.com>
2026-09-21 15:14:28 +0300

    Localize admin: register User model and add app names

--- backend/accounts/admin.py
+++ backend/accounts/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .models import User
+
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    list_display = ('username', 'email', 'phone', 'is_staff', 'date_joined')
+    fieldsets = UserAdmin.fieldsets + (('Дополнительно',
+                                        {'fields': ('phone',)}),)
+    add_fieldsets = UserAdmin.add_fieldsets + (('Дополнительно', {'fields':
+                                                                  ('phone',)}),
+                                               )
