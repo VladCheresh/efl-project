a640c9104aef6b715fe5a281a6890697650b1b3a
vladvin47 <cherry194415@gmail.com>
2026-09-21 12:35:44 +0300

    Fix database host

--- backend/vdzh/settings.py
+++ backend/vdzh/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'NAME': os.environ.get('POSTGRES_DB', 'vdzh'),
         'USER': os.environ.get('POSTGRES_USER', 'vdzh'),
         'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
-        'HOST': os.environ.get('POSTGRES_HOST', 'localhost'),
+        'HOST': os.environ.get('POSTGRES_HOST', '127.0.0.1'),
         'PORT': os.environ.get('POSTGRES_PORT', '5432'),
     }
 }
